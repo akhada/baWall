edb8e2822f7d5238e6843ccaf22283310b383ba7
Anay Nayak <anayak@thoughtworks.com>
2012-09-11 22:29:21 +0530

    Added dummy onclick listeners along with text input for issue number search

--- src/com/example/HomeActivity.java
+++ src/com/example/HomeActivity.java
@@ -2,11 +2,40 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements TextView.OnEditorActionListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        EditText issueNumber = (EditText) findViewById(R.id.issueNumber);
+        issueNumber.setOnEditorActionListener(this);
+    }
+
+    public void onCameraCapture(View view) {
+        Toast.makeText(this, "You wish!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onKeyboardCapture(View view) {
+        Toast.makeText(this, "You wish!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onGestureCapture(View view) {
+        Toast.makeText(this, "You wish!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            Toast.makeText(this, "Search jira", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
     }
 }
