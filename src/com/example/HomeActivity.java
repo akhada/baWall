ff035459e573c4ffa350fe3e7f337c9d6c7e45b5
Anay Nayak <anayak@thoughtworks.com>
2012-09-11 21:38:07 +0530

    Adding initial home screen with basic icons/buttons

--- src/com/example/HomeActivity.java
+++ src/com/example/HomeActivity.java
@@ -3,12 +3,9 @@ package com.example;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class HomeActivity extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
